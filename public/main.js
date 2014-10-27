7571ad235d116b8ef2cbd0fd82c092bfa685403e
Mikey Badr <mfbadr@gmail.com>
2014-10-27 10:58:32 -0500

    animation and sound

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 //init phaser, and create a 400x490px game
 
-var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(800, 490, Phaser.AUTO, 'gameDiv');
 
 //main state
 
@@ -14,6 +14,9 @@ var mainState = {
 
     //load pipe
     game.load.image('pipe', '/assets/pipe.png');
+
+    //load sounds
+    game.load.audio('jump', '/assets/jump.wav');
 
   },
 
@@ -38,10 +41,17 @@ var mainState = {
     this.pipes.enableBody = true; //add physics to the group;
     this.pipes.createMultiple(20, 'pipe'); //make 20 pipes
 
-    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+    //generate pipes on time
+    this.timer = game.time.events.loop(800, this.addRowOfPipes, this);
 
+    //score and update score
     this.score = 0;
     this.labelScore = game.add.text(20, 20, "0", {font: "30px Arial", fill: "#ffffff"});
+
+    //correct anchor for bird animation
+    this.bird.anchor.setTo(-0.2, 0.5);
+
+    this.jumpSound = game.add.audio('jump');
 
   },
 
@@ -53,7 +63,7 @@ var mainState = {
     pipe.reset(x, y);
 
     //give it velocity
-    pipe.body.velocity.x = -200;
+    pipe.body.velocity.x = -800;
 
     //kill pipe when it's no longer visible
     pipe.checkWorldBounds = true;
@@ -66,8 +76,8 @@ var mainState = {
 
     //ad the 6 pipes
     for (var i = 0; i < 8; i++)
-      if (i != hole && i != hole + 1)
-        this.addOnePipe(400, i * 60 + 10);
+      if (i != hole && i != hole + 1 && i != hole +2)
+        this.addOnePipe(800, i * 60 + 10);
 
     this.score += 1;
     this.labelScore.text = this.score;
@@ -78,11 +88,39 @@ var mainState = {
       this.restartGame();
     }
 
-    game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
+    game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
+
+    if (this. bird.angle < 20)
+      this.bird.angle += 1;
+  },
+
+  hitPipe: function(){
+    //if no collision, do nothing
+    if (this.bird.alive == false)
+      return;
+
+    //kill bird
+    this.bird.alive = false;
+    game.time.events.remove(this.timer);
+
+    this.pipes.forEachAlive(function(p){
+      p.body.velocity.x = 0;
+    }, this);
   },
 
   jump: function(){
+    if(this.bird.alive == false)
+      return;
+
     this.bird.body.velocity.y = -350;
+
+    var animation = game.add.tween(this.bird);
+
+    animation.to({angle: -20}, 100);
+
+    animation.start();
+
+    this.jumpSound.play();
   },
 
   restartGame: function(){
